8e98cfc765792589db082ebf40d656b957aeb5e4
Bis-kunal09 <68540759+Bis-kunal09@users.noreply.github.com>
2022-08-17 12:33:48 +0530

    Time: 4 ms (71.25%), Space: 8.5 MB (88.09%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        map<char,string> m;
-        vector<string> code={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        for(char i='a';i<='z';i++){
-            m[i]=code[i-'a'];
+        vector<string> morse_code = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_set<string> gen_codes;
+
+        for(auto word : words) {
+            string code = "";
+            for(auto ch : word)
+                code += morse_code[ch - 'a'];
+            gen_codes.insert(code);
         }
-        vector<string> ans;
-        for(auto x:words){
-            string s="";
-            for(auto i:x){
-                s+=m[i];
-            }
-            ans.push_back(s);
-        }
-        int fans=1;
-        sort(ans.begin(),ans.end());
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]!=ans[i-1]){
-                fans++;
-            }
-        }
-        return fans;
-        
+
+        return gen_codes.size();
     }
 };
